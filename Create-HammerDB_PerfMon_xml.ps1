82cfa4c57a837dcfd684c556c3705fc601c9f2f6
Kevin MacKay <kevin.mackay@d2l.com>
2023-08-29 15:04:37 -0400

    Adding files

--- Create-HammerDB_PerfMon_xml.ps1
+++ Create-HammerDB_PerfMon_xml.ps1
@@ -1,6 +1,8 @@
 <#
+For 
+.\Create-HammerDB_PerfMon_xml.ps1 -Server LAB1APSQLX466 -Folder C:\gitrepos\Demo-Repo -DurationInMinutes 5
 For FCI
-Create-HammerDB_PerfMon_xml.ps1 -Server "LAB1APSQLA499" -SqlInstanceName "INSTA" -Folder "c:\Perfmon" -DurationInMinutes 10
+.\Create-HammerDB_PerfMon_xml.ps1 -Server "LAB1APSQLA499" -SqlInstanceName "INSTA" -Folder C:\gitrepos\Demo-Repo -DurationInMinutes 10
 #>
 
  
@@ -18,13 +20,22 @@ else {
     $SqlInstanceName = "MSSQL`$$SqlInstanceName"
 }
 
-$TimeInSec = $DurationInMinutes*60
+$DriveLetters = Get-WmiObject win32_logicaldisk | select -expandproperty caption
+$DriveLetters = $DriveLetters.trim(":")
+foreach ($Driveletter in $DriveLetters){
+$DiskNumber = Get-Partition -DriveLetter $DriveLetter | select -ExpandProperty DiskNumber
+$Text1 += "<Counter>\\$Server\PhysicalDisk("+ $DiskNumber.ToString() +" "+ $Driveletter +":)\Disk Reads/sec</Counter>`n"
+$Text1 += "<Counter>\\$Server\PhysicalDisk("+ $DiskNumber.ToString() +" "+ $Driveletter +":)\Disk Writes/sec</Counter>`n"
+$Text2 += "<CounterDisplayName>\\$Server\PhysicalDisk("+ $DiskNumber.ToString() +" "+ $Driveletter +":)\Disk Reads/sec</CounterDisplayName>`n"
+$Text2 += "<CounterDisplayName>\\$Server\PhysicalDisk("+ $DiskNumber.ToString() +" "+ $Driveletter +":)\Disk Writes/sec</CounterDisplayName>`n"
+ }
 
+$TimeInSec = $DurationInMinutes * 60
 
-    $ConfigTemp = Get-Content $Folder\Hammerdb_Perfmon_template.xml -Raw
-    $CustomConfig = $ExecutionContext.InvokeCommand.ExpandString($ConfigTemp)
-    $CustomConfig | Out-File $Folder\Hammerdb_Perfmon_$Server.xml
+$ConfigTemp = Get-Content $Folder\Hammerdb_Perfmon_template.xml -Raw
+$CustomConfig = $ExecutionContext.InvokeCommand.ExpandString($ConfigTemp)
+$CustomConfig | Out-File $Folder\Hammerdb_Perfmon_$Server.xml
 
-    Notepad $Folder\Hammerdb_Perfmon_$Server.xml
+Notepad $Folder\Hammerdb_Perfmon_$Server.xml
     
 
